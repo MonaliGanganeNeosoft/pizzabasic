331ff6d8dc823fac85b093ba941ca16292a90677
MoniNeo <moni.gangane@gmail.com>
2022-02-26 21:25:34 +0530

    for basic signup  form with validation frontside go to login

--- pizza/backend/server.js
+++ pizza/backend/server.js
@@ -1,10 +1,13 @@
 const express=require('express')
 const app=express();
 const port=8888;
+const cors=require('cors');
 
 require("./db/connection")
 const userRoutes=require("./routes/userRoutes")
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
 app.use(userRoutes)
 
 app.get('/',(req,res)=>{
--- pizza/frontend/src/config/Myservice.js
+++ pizza/frontend/src/config/Myservice.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { My_Url } from "./Url";
-let token=localStorage.getItem(_token);
+// let token=localStorage.getItem(_token);
 export function addSignup(data){
-    return axios.post(`${My_Url}/signup`,data)
+    return axios.post(`${My_Url}signup`,data)
 }
 export function login(data){
     return axios.post(`${My_Url}login`,data)
