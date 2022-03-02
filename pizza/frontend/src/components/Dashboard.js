542d1c10986301f65f6d953aa304ae39ddabb756
MoniNeo <moni.gangane@gmail.com>
2022-03-02 17:03:35 +0530

    product added to cart localstorage used

--- pizza/frontend/src/components/Dashboard.js
+++ pizza/frontend/src/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React,{useEffect, useState} from 'react'
-import { Card,Container, Row } from 'react-bootstrap';
+import { Button, Card,Container, Row } from 'react-bootstrap';
 import {getPizzaData} from "../config/Myservice"
+import {useDispatch,useSelector} from "react-redux";
 
 function Dashboard() {
   const[prodata,setProdata]=useState([]);
+  const[cart,setCart]=useState([]);
+  const dispactch=useDispatch();
+  const [uid,setUid]=useState();
   useEffect(()=>{
     getPizzaData().then((res)=>{
       console.log(res.data)
@@ -12,6 +16,47 @@ function Dashboard() {
       }
     })
   },[])
+  const addCart=(id,prod)=>{
+    //console.log("click")
+    let obj={id:prod.id,pname:prod.pname,price:prod.price,image:prod.image,quantity:1};
+    console.log(obj);
+    console.log(cart);
+    console.log(prod);
+    if(localStorage.getItem("cart")){
+      let array = JSON.parse(localStorage.getItem("cart"));
+      let found = array.some((cart)=>cart["id"]===id);
+      if(found == true){
+        alert("Product alreday added");
+        if(JSON.parse(localStorage.getItem("cart"))){
+          const count=JSON.parse(localStorage.getItem("cart"))
+            .map((item)=>Number(item.quantity))
+            .reduce((prev,curr)=>prev+curr,0);
+          dispactch({type:"count",payload:count});
+        }
+      }else{
+        array.push(obj);
+        localStorage.setItem("cart",JSON.stringify(array));
+        const count = JSON.parse(localStorage.getItem("cart"))
+          .map((item)=>Number(item.quantity))
+          .reduce((prev,curr)=>prev+curr,0);
+        dispactch({type:"count",payload:count});
+        alert("Product added to cart");
+          }
+      }else{
+        let array=[];
+        array.push(obj);
+        localStorage.setItem("cart",JSON.stringify(array));
+        if(JSON.parse(localStorage.getItem("cart"))){
+            const count = JSON.parse(localStorage.getItem("cart"))
+              .map((item)=>Number(item.quantity))
+              .reduce((prev,curr)=>prev+curr,0);
+            console.log("count"+count);
+            dispactch({type:"count",payload:count});
+            }
+        alert("Product added to cart")
+      }
+
+  };
   return (
     <>
     <Container className="mt-4">
@@ -19,6 +64,11 @@ function Dashboard() {
         {prodata.length > 0 && prodata.map((prod,i)=>
           <Card key={i} style={{width:"18rem",margin:"10px"}} className="bg-light bg-gradient">
             <Card.Img variant="top" src={prod.image} className="mt-1 bg-gradient" width="200" height="250"  />
+            <Card.Body>
+              <Card.Title>{prod.pname}</Card.Title>
+              <Card.Text>{prod.price}<b>Rs</b></Card.Text>
+              <Button variant='primary' onClick={()=>addCart(prod.id,prod)}>Add to cart</Button>
+            </Card.Body>
           </Card>)}
       </Row>
     </Container>
